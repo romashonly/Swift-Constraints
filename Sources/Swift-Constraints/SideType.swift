1fe51cbdc36ee6ed079620962c3811ad9e3ebb37
Roman <roma.shurkin@mail.ru>
2022-10-19 01:29:29 +0300

    implemented

---
+++ Sources/Swift-Constraints/SideType.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Роман Шуркин on 19.10.2022.
+//
+
+import Foundation
+
+public enum SideType: Int {
+    case top = 0
+    case bottom
+    case left
+    case right
+    
+    case centerX
+    case centerY
+    
+    case height
+    case width
+    
+    case edges
+    case size
+}
